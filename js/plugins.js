3b8728191a65d84f37b79954f7bf5f8169e72fee
Baadeababae <266558527+cyruscylinder22@users.noreply.github.com>
2026-05-05 22:52:35 +0100

    Add files via upload

--- js/plugins.js
+++ js/plugins.js
@@ -8,5 +8,7 @@ var $plugins =
 {"name":"GALV_PuzzleFunctionsMZ","status":false,"description":"(v.1.1) A bunch of functions to use to make eventing puzzles easier\r\nView the plugin \"Help\" to view available commands.","parameters":{}},
 {"name":"IceSlide","status":true,"description":"Ice Slide: Plays exactly 3 animation frames, then stays static.","parameters":{"IceTerrainTag":"2"}},
 {"name":"SlidingPuzzle","status":true,"description":"4x4 Sliding Puzzle - High Priority Click Logic.","parameters":{"StartX":"24","StartY":"28","WinSwitch":"24"}},
-{"name":"QuicksandSystem","status":true,"description":"Smooth 5px Quicksand: Visual cropping with Global Terrain Tags.","parameters":{"DefaultRegions":"[\"3\"]","DefaultTerrains":"[\"3\"]","DamageAmount":"50"}}
+{"name":"QuicksandSystem","status":true,"description":"Smooth 5px Quicksand: Visual cropping with Global Terrain Tags.","parameters":{"DefaultRegions":"[\"3\"]","DefaultTerrains":"[\"3\"]","DamageAmount":"50"}},
+{"name":"ScreenZoom","status":true,"description":"[Ver1.1.1]Zoom screen whose center is specified character or player.","parameters":{}},
+{"name":"TallSpriteRegionFix","status":true,"description":"Boosts Sprite Z-Priority based on Region ID or Terrain Tag.","parameters":{"BoostRegionId":"0","BoostTerrainTag":"7"}}
 ];
